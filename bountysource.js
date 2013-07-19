94dfa4f58563d3c80f77d6deaa9c66203b16f425
thefinn93 <thefinn93@thefinn93.com>
2013-07-19 00:53:46 -0700

    added issue list support

--- bountysource.js
+++ bountysource.js
@@ -1,6 +1,5 @@
-function checkBounty(url, cb) {
-    bounty = false
-    $.get("https://api.bountysource.com/search?_method=POST&query=" + encodeURIComponent(url)).done(function(redir) {
+function issuePage() {
+    $.get("https://api.bountysource.com/search?_method=POST&query=" + encodeURIComponent(window.location.href)).done(function(redir) {
         if(redir.hasOwnProperty("redirect_to")) {
             issueurl = "https://api.bountysource.com" + redir['redirect_to'].replace("#","/");
             $.get(issueurl).done(function(data) {
@@ -13,7 +12,28 @@ function checkBounty(url, cb) {
             });
         }
     });
-    return bounty;
 }
 
-bounty = checkBounty(window.location.href);
+function issueList() {
+    $('.list-group-item').each(function(index) {
+        var issue = $(this.getElementsByTagName("li")[0]);
+        var issueURL = this.getElementsByClassName("js-navigation-open")[0].href;
+        $.get("https://api.bountysource.com/search?_method=POST&query=" + encodeURIComponent(issueURL)).done(function(redir) {
+            if(redir.hasOwnProperty("redirect_to")) {
+                var issueurl = "https://api.bountysource.com" + redir['redirect_to'].replace("#","/");
+                $.get(issueurl).done(function(data) {
+                    issue.append("<a href=\"" + data['frontend_url'] + "\">$" + data['bounty_total'] + " bounty</a>");
+                });
+            }
+        });
+    });
+}
+
+regex = {}
+regex['issuePage'] = /https:\/\/github.com\/.*\/.*\/issues\/[0-9]*/
+regex['issueList'] = /https:\/\/github.com\/.*\/.*\/issues(?!\/).*/
+if(regex['issuePage'].test(window.location.href)) {
+    issuePage();
+} else if(regex['issueList'].test(window.location.href)) {
+    issueList();
+}
