3c057820cfa2f1a2ca980264bfed5829e56a389e
thefinn93 <thefinn93@thefinn93.com>
2013-07-19 02:34:42 -0700

    Cleaned things up, experimented with jquery events, failed

--- bountysource.js
+++ bountysource.js
@@ -1,8 +1,8 @@
 function issuePage() {
-    $.get("https://api.bountysource.com/search?_method=POST&query=" + encodeURIComponent(window.location.href)).done(function(redir) {
+    $.get("https://api.bountysource.com/search?_method=POST&query=" + encodeURIComponent(window.location.href), {global: false}).done(function(redir) {
         if(redir.hasOwnProperty("redirect_to")) {
             issueurl = "https://api.bountysource.com" + redir['redirect_to'].replace("#","/");
-            $.get(issueurl).done(function(data) {
+            $.get(issueurl, {global: false}).done(function(data) {
                 //data['bounty_total'];
                 $('.discussion-stats')
                     .append($("<span class=\"state-indicator bounty\"></span>")
@@ -15,13 +15,13 @@ function issuePage() {
 }
 
 function issueList() {
-    $('.list-group-item').each(function(index) {
+    $('.issue-list-item').each(function(index) {
         var issue = $(this.getElementsByTagName("li")[0]);
         var issueURL = this.getElementsByClassName("js-navigation-open")[0].href;
-        $.get("https://api.bountysource.com/search?_method=POST&query=" + encodeURIComponent(issueURL)).done(function(redir) {
+        $.get("https://api.bountysource.com/search?_method=POST&query=" + encodeURIComponent(issueURL), {global: false}).done(function(redir) {
             if(redir.hasOwnProperty("redirect_to")) {
                 var issueurl = "https://api.bountysource.com" + redir['redirect_to'].replace("#","/");
-                $.get(issueurl).done(function(data) {
+                $.get(issueurl, {global: false}).done(function(data) {
                     issue.append("<a href=\"" + data['frontend_url'] + "\">$" + data['bounty_total'] + " bounty</a>");
                 });
             }
@@ -29,11 +29,17 @@ function issueList() {
     });
 }
 
-regex = {}
-regex['issuePage'] = /https:\/\/github.com\/.*\/.*\/issues\/[0-9]*/
-regex['issueList'] = /https:\/\/github.com\/.*\/.*\/issues(?!\/).*/
-if(regex['issuePage'].test(window.location.href)) {
-    issuePage();
-} else if(regex['issueList'].test(window.location.href)) {
-    issueList();
-}
+function checkPage(a,b,url) {
+    regex = {}
+    regex['issuePage'] = /https:\/\/github.com\/.*\/.*\/issues\/[0-9]*/
+    regex['issueList'] = /https:\/\/github.com\/.*\/.*\/issues(?!\/).*/
+    if(regex['issuePage'].test(window.location.href)) {
+        issuePage();
+    } else if(regex['issueList'].test(window.location.href)) {
+        issueList();
+    }
+};
+
+$(document).ready(function() {
+    checkPage();
+    });
